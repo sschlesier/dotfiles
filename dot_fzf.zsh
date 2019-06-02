b5fcf22f6df8a77e0a5f32d959d577e488f47e9f
Scott S <scott@schlesier.ca>
2019-06-02 11:10:54 -0700

    fzf.zsh to support mac and linux folder

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -1,13 +1,22 @@
+# Where is fzf installed
+if [[ -d /usr/share/fzf ]]; then
+  prefix=/usr/share/fzf
+elif [[ -d /usr/local/opt/fzf ]]; then
+  prefix=/usr/local/opt/fzf
+else
+  exit
+fi
+
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == *$prefix/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$prefix/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$prefix/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$prefix/shell/key-bindings.zsh"

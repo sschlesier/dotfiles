83b0c532baa6842586c23f0353b00625e3463144
Scott S <scotts@iqmetrix.com>
2019-06-07 16:55:36 -0700

    add funcs.zsh

---
+++ dot_zsh/funcs.zsh
@@ -0,0 +1,18 @@
+#make directory and cd to it
+mcd() {
+  mkdir "$1"
+  cd "$1"
+}
+export mcd
+
+#make director w/ current date
+mdd() {
+  mcd $(date '+%Y-%m-%d')
+}
+export mdd
+
+#open the current folder in explorer
+b() {
+	open_command .
+}
+export b

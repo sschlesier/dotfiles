3b5552b0277813454c7114301b5affe8a06e6317
Scott S <scott@schlesier.ca>
2019-06-08 22:02:59 -0700

    forget shell_environment.zsh

--- dot_zsh/shell_environment.zsh
+++
@@ -1,4 +0,0 @@
-export EMAIL=scotts@iqmetrix.com
-export LAUNCH_TMUX=1
-export INSECURE_DOCKER=1
-export CRED_HELPER="/mnt/c/Program\\\\ Files/Git/mingw64/libexec/git-core/git-credential-wincred.exe"

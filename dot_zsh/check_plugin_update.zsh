5d79db9ca847b7c76a84ce10aa716acd3a7e0b9a
Scott S <scott@schlesier.ca>
2019-06-15 07:17:34 -0700

    Revert "try plugin update with EPOCHSECONDS and date -r"
    This reverts commit 2259ead06d88bc619e3dca87a635dfc136288b9b.

--- dot_zsh/check_plugin_update.zsh
+++ dot_zsh/check_plugin_update.zsh
@@ -1,15 +1,11 @@
 #! /usr/bin/evn zsh
 
-zmodload zsh/datetime
-
-daysTillUpdate=7
 update_file="$ZSH_CACHE_DIR/plugins_updated"
 
-let "targetTime = $EPOCHSECONDS - (60 * 60 * 24 * $daysTillUpdate)"
-
 if [[ -f "$update_file" ]]; then
-  lastUpdated=$(date -r $update_file +%s)
-  if [[ $lastUpdated < $targetTime ]]; then
+  oldfile=$(find $ZSH_CACHE_DIR -maxdepth 1 -name "plugins_updated" -mtime +1w -print -quit)
+
+  if [[ -n $oldfile ]]; then
     read "REPLY?Would you like to update all plugins? "
     if [[ $REPLY =~ ^[Yy]$ ]]; then
       update_all_plugins

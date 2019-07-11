7a8214ca52e198afe247246e035903b47dae9d6f
Scott S <scotts@iqmetrix.com>
2019-07-10 19:49:34 -0700

    move dash to be a shell script

--- dot_zsh/funcs.zsh
+++ dot_zsh/funcs.zsh
@@ -1,13 +1,13 @@
 #make directory and cd to it
 mcd() {
-  mkdir "$1"
-  cd "$1"
+	mkdir "$1"
+	cd "$1"
 }
 export mcd
 
 #make directory w/ current date
 mdd() {
-  mcd $(date '+%Y-%m-%d')
+	mcd $(date '+%Y-%m-%d')
 }
 export mdd
 
@@ -22,11 +22,11 @@ alias _='sudo'
 
 # expand command via C-Alt-a
 expand-aliases() {
-  unset 'functions[_expand-aliases]'
-  functions[_expand-aliases]=$BUFFER
-  (($+functions[_expand-aliases])) &&
-    BUFFER=${functions[_expand-aliases]#$'\t'} &&
-    CURSOR=$#BUFFER
+unset 'functions[_expand-aliases]'
+functions[_expand-aliases]=$BUFFER
+(($+functions[_expand-aliases])) &&
+	BUFFER=${functions[_expand-aliases]#$'\t'} &&
+	CURSOR=$#BUFFER
 }
 
 #turn alias into full command at prompt
@@ -34,25 +34,20 @@ zle -N expand-aliases
 bindkey '\e^A' expand-aliases
 
 darkmode() {
-  it2prof gruvbox-dark
+	it2prof gruvbox-dark
 }
 
 lightmode() {
-  it2prof papercolor-light
+	it2prof papercolor-light
 }
 
 # Change iterm2 profile. Usage it2prof ProfileName (case sensitive)
 it2prof() {
-  echo -e "\033]50;SetProfile=$1\a"
-  export ITERM_PROFILE=$1
+	echo -e "\033]50;SetProfile=$1\a"
+	export ITERM_PROFILE=$1
 }
 
 #update tmux environment from current server state
 tmux-update-env() {
-  source <(tmux show-environment -s)
-}
-
-#search dash
-dash() {
-	open dash://"$1"
+source <(tmux show-environment -s)
 }

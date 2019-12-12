596eb2e3eb98e1d949f1bf2e5ec79c48d1197154
Scott S <scott@schlesier.ca>
2019-12-12 13:23:05 -0800

    improve comments on auto margin usage

--- dot_zsh/gitenv.zsh
+++ dot_zsh/gitenv.zsh
@@ -25,8 +25,8 @@ EOF
 fi #[[ ! -f $gitenv ]];
 
 #add'l git tools
-# \033?7l disables automargin like tput rmam but works in tmux
-# \033?7h enables automargin like tput smam but works in tmux
+# \033?7l disables automargin like tput rmam (reset mode auto margin) but works in tmux
+# \033?7h enables automargin like tput smam (set mode auto margin) but works in tmux
 alias gt='printf "\033[?7l$(git tree --color=always | head -n$(($LINES - 4)))\033[?7h"'
 alias gta='git tree --all'
 unalias grv #expose the git repo viewer tool

41f3c4bab9988382a55962e7d06b94d705bd0611
Scott S <scotts@iqmetrix.com>
2020-02-08 07:28:08 -0800

    gt has an empty line at the end

--- dot_zsh/gitenv.zsh
+++ dot_zsh/gitenv.zsh
@@ -4,7 +4,7 @@ gitenv="$ZSH_CACHE_DIR/gitconfig-env"
 if [[ ! -f $gitenv ]]; then
   echo setting up git include
 
-cat <<EOF > "$HOME/.git-indirection" 
+cat <<EOF > "$HOME/.git-indirection"
 [include]
   path = $gitenv
 EOF
@@ -27,7 +27,7 @@ fi #[[ ! -f $gitenv ]];
 #add'l git tools
 # \033?7l disables automargin like tput rmam (reset mode auto margin) but works in tmux
 # \033?7h enables automargin like tput smam (set mode auto margin) but works in tmux
-alias gt='printf "\033[?7l$(git tree --color=always | head -n$(($LINES - 4)))\033[?7h"'
+alias gt='printf "\033[?7l$(git tree --color=always | head -n$(($LINES - 4)))\033[?7h\n"'
 alias gta='git tree --all'
 unalias grv #expose the git repo viewer tool
 alias gh='gitit'

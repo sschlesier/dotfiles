56b91938b4488fc2579efb38636ddf7f7cc9d25c
Scott S <scotts@iqmetrix.com>
2020-06-18 16:33:53 -0700

    dir-aliases from multiple root dirs

--- dot_zsh/dir-aliases.zsh
+++ dot_zsh/dir-aliases.zsh
@@ -4,53 +4,70 @@ aliasdir() {
 	fi
 }
 
-if [[ -d "$C_ROOT"/src ]]; then
-	export SRC="$C_ROOT"/src
-elif [[ -d "$HOME"/src ]]; then
-	export SRC="$HOME"/src
-fi
+srcdir() {
+	if [[ -n $ALTSRC ]]; then
+		aliasdir "$1" "$ALTSRC"/"$2"
+	fi
+	#let main $SRC override alt if they both exist
+	aliasdir "$1" "$SRC"/"$2"
+}
+
+setsrcdirs() {
+	if [[ -z $SRC ]] && [[ -d $1 ]]; then
+		export SRC="$1"
+	elif [[ -z $ALTSRC ]] && [[ -d $1 ]]; then
+		export ALTSRC="$1"
+	fi
+}
+
+setsrcdirs "$C_ROOT"/src
+setsrcdirs "$HOME"/src
 
 #cdpath
 cdpath=("$SRC" "$HOME")
 if [[ -n $WIN_HOME ]]; then
 	cdpath+=("$WIN_HOME")
 fi
+if [[ -n $ALTSRC ]]; then
+	cdpath+=("$ALTSRC")
+fi
 zstyle ':completion:*:complete:(cd|pushd):*' tag-order 'local-directories named-directories'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*:descriptions' format %B%d%b
 
-aliasdir ams "$SRC/AssetMediaService"
-aliasdir ap "$SRC/IQ.Auth.Packages"
-aliasdir api "$SRC/Azure.APIManagement"
-aliasdir au "$SRC/Auth"
-aliasdir av "$SRC/IQ.Platform.Availability"
-aliasdir bsn "$SRC/BuildScripts.Net"
-aliasdir ca "$SRC/Catalogs"
-aliasdir cc "$SRC/CodingChallenge1"
-aliasdir cn "$SRC/CatalogNexus"
-aliasdir cost "$SRC/IQ.Platform.Cost"
-aliasdir crm "$SRC/IQ.Platform.CRM"
 aliasdir de "$DESKTOP"
 aliasdir dow "$HOME/Downloads"
 aliasdir dow "$WIN_HOME/Downloads"
 aliasdir dr "$HOME/Dropbox"
 aliasdir dr "$WIN_HOME/Dropbox"
-aliasdir es "$SRC/EntityStore"
-aliasdir exp "$SRC/experiment"
-aliasdir fl "$SRC/Foundation.Logging"
-aliasdir fm "$SRC/Foundation.Messaging"
-aliasdir fo "$SRC/Foundation.Outbox"
-aliasdir fw "$SRC/Foundation.WebApi"
-aliasdir im "$SRC/IQ.Messaging"
-aliasdir mg "$SRC/Monitoring.Graphite"
 aliasdir od "$WIN_HOME/OneDrive"
-aliasdir pe "$SRC/product-excellence"
-aliasdir pl "$SRC/ProductLibrary"
-aliasdir sbm "$SRC/ServiceBusMonitor"
 aliasdir src "$SRC"
-aliasdir ss "$SRC/shell-setup"
-aliasdir sub "$SRC/ProductSubscriptions"
 aliasdir to "$WIN_HOME/tools"
+
+srcdir ams AssetMediaService
+srcdir ap IQ.Auth.Packages
+srcdir api Azure.APIManagement
+srcdir au Auth
+srcdir av IQ.Platform.Availability
+srcdir bsn BuildScripts.Net
+srcdir ca Catalogs
+srcdir cc CodingChallenge1
+srcdir cn CatalogNexus
+srcdir cost IQ.Platform.Cost
+srcdir crm IQ.Platform.CRM
+srcdir es EntityStore
+srcdir exp experiment
+srcdir fl Foundation.Logging
+srcdir fm Foundation.Messaging
+srcdir fo Foundation.Outbox
+srcdir fw Foundation.WebApi
+srcdir im IQ.Messaging
+srcdir mg Monitoring.Graphite
+srcdir pe product-excellence
+srcdir pl ProductLibrary
+srcdir sbm ServiceBusMonitor
+srcdir ss shell-setup
+srcdir sub ProductSubscriptions
 
 # List directory contents
 if type exa &> /dev/null; then

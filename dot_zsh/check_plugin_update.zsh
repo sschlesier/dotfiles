8f806e94861461d474206d83bab47101334690ee
Scott S <scotts@iqmetrix.com>
2020-10-27 11:23:39 -0700

    check for updates weekly

--- dot_zsh/check_plugin_update.zsh
+++ dot_zsh/check_plugin_update.zsh
@@ -5,9 +5,9 @@ lastUpdatedPath="$ZSH_CACHE_DIR/$lastUpdatedFileName"
 
 #find is faster than fd for this purpose but Fedora doesn't seem to have find
 if type find > /dev/null; then
-  foundFile=$(find $ZSH_CACHE_DIR -maxdepth 1 -name $lastUpdatedFileName -mtime +14 -print -quit)
+  foundFile=$(find $ZSH_CACHE_DIR -maxdepth 1 -name $lastUpdatedFileName -mtime +7 -print -quit)
 elif type fd > /dev/null; then
-  foundFile=$(fd --max-depth=1 --change-older-than 2weeks $lastUpdatedFileName "$ZSH_CACHE_DIR")
+  foundFile=$(fd --max-depth=1 --change-older-than 1weeks $lastUpdatedFileName "$ZSH_CACHE_DIR")
 else
   echo unable to refresh plugins add 'find' or 'fd' to your path
 fi

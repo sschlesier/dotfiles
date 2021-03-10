88fb541d7a717e02856e6c8ee09ef3fa6fe7c349
Scott S <scott@schlesier.ca>
2021-03-10 13:43:11 -0800

    only update plugins inside tmux
    and start moving away from zsh specific
    config

--- dot_zsh/check_plugin_update.sh
+++ dot_zsh/check_plugin_update.sh
@@ -1,19 +1,24 @@
-#! /usr/bin/evn zsh
+#! /usr/bin/env bash
 
 lastUpdatedFileName=plugins_updated
 lastUpdatedPath="$ZSH_CACHE_DIR/$lastUpdatedFileName"
 
+#only run when in TMUX as the tmux updates will fail outside of tmux
+if [[ -z $TMUX ]]; then
+	return
+fi
+
 #find is faster than fd for this purpose but Fedora doesn't seem to have find
 if type find > /dev/null; then
-  foundFile=$(find $ZSH_CACHE_DIR -maxdepth 1 -name $lastUpdatedFileName -mtime +7 -print -quit)
+  foundFile=$(find "$ZSH_CACHE_DIR" -maxdepth 1 -name "$lastUpdatedFileName" -mtime +7 -print -quit)
 elif type fd > /dev/null; then
-  foundFile=$(fd --max-depth=1 --change-older-than 1weeks $lastUpdatedFileName "$ZSH_CACHE_DIR")
+  foundFile=$(fd --max-depth=1 --change-older-than 1weeks "$lastUpdatedFileName" "$ZSH_CACHE_DIR")
 else
   echo unable to refresh plugins add 'find' or 'fd' to your path
 fi
 
 if [[ -n "$foundFile" ]]; then
-  read "CONFIRM?Would you like to update all plugins? "
+  read  -r "CONFIRM?Would you like to update all plugins? "
   if [[ $CONFIRM =~ ^[Yy]$ ]]; then
     update_all_plugins
     touch "$lastUpdatedPath"

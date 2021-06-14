d425c52ba32f15ca958b8b48b1d31e5c758f68b2
Scott S <scott@schlesier.ca>
2021-06-14 01:22:32 +0000

    update nix-userspace to use a .nix file

---
+++ bin/userspace-shell.nix
@@ -0,0 +1,35 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.antibody
+    pkgs.bat
+    pkgs.chezmoi
+    pkgs.diff-so-fancy
+    pkgs.dog
+    pkgs.entr
+    pkgs.exa
+    pkgs.fd
+    pkgs.file
+    pkgs.fzf
+    pkgs.httpie
+    pkgs.hyperfine
+    pkgs.jq
+    pkgs.lazygit
+    pkgs.neovim
+    pkgs.nodePackages.npm
+    pkgs.nodejs
+    pkgs.parallel
+    pkgs.shellcheck
+    pkgs.silver-searcher
+    pkgs.tealdeer
+    pkgs.tmux
+    pkgs.wget
+    pkgs.zsh
+  ];
+
+  shellHook = ''
+    export SHELL=$(which zsh)
+    zsh
+  '';
+}

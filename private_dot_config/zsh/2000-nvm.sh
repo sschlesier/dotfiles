b50f8f7df6cf6644666467d5f90f854914755279
Scott S <scotts@readytopay.com>
2021-12-09 20:00:53 -0800

    add nvm script

---
+++ private_dot_config/zsh/2000-nvm.sh
@@ -0,0 +1,4 @@
+export NVM_DIR="$HOME/.nvm"
+[ ! -d "$NVM_DIR" ] && mkdir -p "$NVM_DIR"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"

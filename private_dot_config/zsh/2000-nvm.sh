aeb3561e48d297935aea83f7260a4920ed90a59a
Scott Schlesier <scott@schlesier.ca>
2021-12-09 20:08:07 -0800

    update nvm script to use HOMEBREW_PREFIX

--- private_dot_config/zsh/2000-nvm.sh
+++ private_dot_config/zsh/2000-nvm.sh
@@ -1,4 +1,4 @@
 export NVM_DIR="$HOME/.nvm"
 [ ! -d "$NVM_DIR" ] && mkdir -p "$NVM_DIR"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+[ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && . "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"

167872c3aa00dfac60ee9e703bb4714cbc14e6cf
Scott Schlesier <scott@schlesier.ca>
2022-01-04 10:50:25 -0800

    complete changes for drop OMZ docker plugin

--- private_dot_config/zsh/0100-completion-and-plugins.zsh
+++ private_dot_config/zsh/0100-completion-and-plugins.zsh
@@ -4,26 +4,13 @@ if [[ -n $TMUX ]]; then
   export ZSH_DISABLE_COMPFIX=true
 fi
 
-if [[ -n $HOMEBREW_PREFIX ]]; then
-  FPATH="$HOMEBREW_PREFIX/share/zsh/site-functions:${FPATH}"
-fi
-
-#update plugins when out of date
-plugins_file="$ZSH_CACHE_DIR/pre_compinit_plugins.zsh"
-plugins_src="$ZSH_INIT/pre_compinit_plugins.txt"
-if [[ ! -e $plugins_file || $plugins_file -ot "$plugins_src" ]] && type antibody >/dev/null; then
-  echo updating "$plugins_file" ...
-  antibody bundle < "$plugins_src" > $plugins_file
-fi
-source $plugins_file
-
 autoload -Uz compinit && compinit
 
-plugins_file="$ZSH_CACHE_DIR/post_compinit_plugins.zsh"
-plugins_src="$ZSH_INIT/post_compinit_plugins.txt"
+#update plugins when out of date
+plugins_file="$ZSH_CACHE_DIR/plugins.zsh"
+plugins_src="$ZSH_INIT/plugins.txt"
 if [[ ! -e $plugins_file || $plugins_file -ot "$plugins_src" ]] && type antibody >/dev/null; then
   echo updating "$plugins_file" ...
   antibody bundle < "$plugins_src" > $plugins_file
 fi
 source $plugins_file
-

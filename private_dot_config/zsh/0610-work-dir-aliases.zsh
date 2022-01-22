ae2a423c2aeafa84b074a885675b863bc9f62fa8
Scott Schlesier <scotts@readytopay.com>
2022-01-22 07:52:39 -0800

    add some ready aliases

--- private_dot_config/zsh/0610-work-dir-aliases.zsh
+++ private_dot_config/zsh/0610-work-dir-aliases.zsh
@@ -1,3 +1,8 @@
-srcdir ra Ready.API
+srcdir ra  Ready.API
+srcdir rad Ready.API.Dashboard
+srcdir rd  Ready.Dashboard
+srcdir rda Ready.DashboardV2Api
+srcdir rma Ready.MobileApp
+srcdir rmu Ready.Menu
 
 aliasdir od "$HOME/OneDrive"

6e56d8bfff6e2b2e5f91af83527fd3a7d4d7163c
Scott Schlesier <scotts@readytopay.com>
2022-02-15 19:42:27 -0800

    lazynvm adapted to proper HOMEBREW dirs

--- private_dot_config/zsh/2000-nvm.sh
+++ private_dot_config/zsh/2000-nvm.sh
@@ -1,27 +1,27 @@
 export NVM_DIR="$HOME/.nvm"
 [ ! -d "$NVM_DIR" ] && mkdir -p "$NVM_DIR"
-# lazynvm adapted from 
+# lazynvm adapted from
 # https://til-engineering.nulogy.com/Slow-Terminal-Startup-Tip-Lazy-Load-NVM/
 
 lazynvm() {
   unset -f nvm node npm npx
-  export NVM_DIR=~/.nvm
-  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-  if [ -f "$NVM_DIR/bash_completion" ]; then
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
+  if [ -f "$HOMEBREW_PREFIX/opt/nvm/bash_completion" ]; then
+    [ -s "$HOMEBREW_PREFIX/opt/nvm/bash_completion" ] && \. "$HOMEBREW_PREFIX/opt/nvm/bash_completion" # This loads nvm bash_completion
   fi
 }
 
 nvm() {
-  lazynvm 
+  lazynvm
   nvm $@
 }
- 
+
 node() {
   lazynvm
   node $@
 }
- 
+
 npm() {
   lazynvm
   npm $@

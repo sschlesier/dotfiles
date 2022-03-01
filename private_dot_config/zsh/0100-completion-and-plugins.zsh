d5ebc6dcc7aab8cc74d6b59379a0910998208c05
Scott Schlesier <scott@schlesier.ca>
2022-03-01 05:28:46 -0800

    drop ssh-agent

--- private_dot_config/zsh/0100-completion-and-plugins.zsh
+++ private_dot_config/zsh/0100-completion-and-plugins.zsh
@@ -1,3 +1,12 @@
+if [[ -n $TMUX ]]; then
+  #checking for insecure completion folders is slow so disable it
+  #in tmux sub-shells keep it at startup to notice bad folders
+  export ZSH_DISABLE_COMPFIX=true
+fi
+
+_comp_options+=(globdots) # complete hidden files
+autoload -Uz compinit && compinit
+
 #update plugins when out of date
 plugins_file="$ZSH_CACHE_DIR/plugins.zsh"
 plugins_src="$ZDOTDIR/plugins.txt"

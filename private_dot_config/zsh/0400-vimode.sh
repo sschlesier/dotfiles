0712a30eeba207826c162d92a7b0bcd0bb9fae66
Scott Schlesier <scotts@readytopay.com>
2022-03-29 09:43:34 -0700

    enable v in zsh vimode to invoke $EDITOR

--- private_dot_config/zsh/0400-vimode.sh
+++ private_dot_config/zsh/0400-vimode.sh
@@ -1,4 +1,10 @@
 #enable and configure vi mode
 bindkey -v
+
+# use v to edit in $EDITOR
+autoload edit-command-line
+zle -N edit-command-line
+bindkey -M vicmd v edit-command-line
+
 export KEYTIMEOUT=1	#wait 10ms when switching to vi-mode
 

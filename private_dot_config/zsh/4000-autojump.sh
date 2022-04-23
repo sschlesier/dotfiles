8af4ed1175f3127257031832f3087f62ac3e9003
Scott Schlesier <scott@schlesier.ca>
2022-04-23 09:41:53 -0700

    autojump via cdr

---
+++ private_dot_config/zsh/4000-autojump.sh
@@ -0,0 +1,10 @@
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+zstyle ':chpwd:*' recent-dirs-default yes
+zstyle ':completion:*' recent-dirs-insert always
+alias j=cdr
+
+zstyle ':completion:*' menu select
+
+setopt complete_in_word
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'

f0d2e3001b7e1800a8658aef405def73a691b6a5
Scott Schlesier <scotts@readytopay.com>
2022-11-14 14:59:53 -0800

    drop direnv and lazynvm for straigh nvm and zsh shell integration

--- private_dot_config/zsh/2000-nvm.sh
+++ private_dot_config/zsh/2000-nvm.sh
@@ -1,33 +1,24 @@
 export NVM_DIR="$HOME/.nvm"
 [ ! -d "$NVM_DIR" ] && mkdir -p "$NVM_DIR"
-# lazynvm adapted from
-# https://til-engineering.nulogy.com/Slow-Terminal-Startup-Tip-Lazy-Load-NVM/
+[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+[ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && . "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"
 
-lazynvm() {
-  unset -f nvm node npm npx
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
-  if [ -f "$HOMEBREW_PREFIX/opt/nvm/bash_completion" ]; then
-    [ -s "$HOMEBREW_PREFIX/opt/nvm/bash_completion" ] && \. "$HOMEBREW_PREFIX/opt/nvm/bash_completion" # This loads nvm bash_completion
+# adapted from https://github.com/nvm-sh/nvm#zsh
+autoload -U add-zsh-hook
+loadnvmrc() {
+  local nvmrc_path="$(nvm_find_nvmrc)"
+
+  if [ -n "$nvmrc_path" ]; then
+    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
+
+    if [ "$nvmrc_node_version" = "N/A" ]; then
+      nvm install
+    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
+      nvm use
+    fi
+  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
+    echo "Reverting to nvm default version"
+    nvm use default
   fi
 }
-
-nvm() {
-  lazynvm
-  nvm "$@"
-}
-
-node() {
-  lazynvm
-  node "$@"
-}
-
-npm() {
-  lazynvm
-  npm "$@"
-}
-
-npx() {
-  lazynvm
-  npx "$@"
-}
+add-zsh-hook chpwd loadnvmrc
--- private_dot_config/zsh/3000-direnv.sh
+++
@@ -1,7 +0,0 @@
-# to support use node
-export NODE_VERSIONS=~/.nvm/versions/node
-export NODE_VERSION_PREFIX=v
-
-if type direnv > /dev/null; then
-	eval "$(direnv hook zsh)"
-fi

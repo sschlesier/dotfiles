11ba20ac7b839be4034eb93c9468220771dab274
Scott Schlesier <scott@schlesier.ca>
2022-11-15 21:39:39 -0800

    use zsh-async to async load nvm

--- private_dot_config/zsh/2000-nvm.sh
+++ private_dot_config/zsh/2000-nvm.sh
@@ -1,28 +1,12 @@
-[ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" --no-use
-[ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && . "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"
-
-# adapted from https://github.com/nvm-sh/nvm#zsh
-autoload -U add-zsh-hook
-loadnvmrc() {
-  local nvmrc_path="$(nvm_find_nvmrc)"
-
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
-      nvm use
-    fi
-  elif [ -n "$(PWD=$OLDPWD nvm_find_nvmrc)" ] && [ "$(nvm version)" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
+# adapted from https://github.com/nvm-sh/nvm/issues/539#issuecomment-403661578
+autoload -Uz async && async
+export NVM_DIR="$HOME/.nvm"
+function load_nvm() {
+    [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+    [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && . "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"
 }
-add-zsh-hook chpwd loadnvmrc
 
-# adapted from https://github.com/nvm-sh/nvm/issues/539#issuecomment-245791291
-#startup nvm when needed
-alias node='unalias node ; unalias npm ; unalias yarn ; nvm use default ; node $@'
-alias npm='unalias node ; unalias npm ; unalias yarn ; nvm use default ; npm $@'
-alias yarn='unalias node ; unalias npm ; unalias yarn ; nvm use default ; yarn $@'
+# Initialize worker
+async_start_worker nvm_worker -n
+async_register_callback nvm_worker load_nvm
+async_job nvm_worker sleep 0.1

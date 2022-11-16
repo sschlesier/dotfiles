20c1a919a4a9bd26e9b2f5a33640c99d717406c9
Scott Schlesier <scott@schlesier.ca>
2022-11-16 00:01:19 -0800

    drop redundant nvm export

--- private_dot_config/zsh/2000-nvm.sh
+++ private_dot_config/zsh/2000-nvm.sh
@@ -1,6 +1,5 @@
 # adapted from https://github.com/nvm-sh/nvm/issues/539#issuecomment-403661578
 autoload -Uz async && async
-export NVM_DIR="$HOME/.nvm"
 function load_nvm() {
     [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
     [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && . "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm"

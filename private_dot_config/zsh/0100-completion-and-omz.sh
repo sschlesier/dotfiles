65a2aac1688d0a1c307052f94a6db6a322c70522
Scott Schlesier <scott@schlesier.ca>
2022-11-16 00:10:52 -0800

    async load completion and omz plugins

---
+++ private_dot_config/zsh/0100-completion-and-omz.sh
@@ -0,0 +1,25 @@
+autoload -Uz compinit
+
+#preload async
+async_init=$(echo mafredri/zsh-async | antibody bundle)
+eval $async_init
+
+autoload -Uz async && async
+
+function load_completion() {
+    _comp_options+=(globdots) # complete hidden files
+    compinit
+
+    #omz requires compdef to be defined
+    plugins_file="$ZSH_CACHE_DIR/omz-plugins.zsh"
+    plugins_src="$ZDOTDIR/omz-plugins.txt"
+    if [[ ! -e $plugins_file || $plugins_file -ot $plugins_src ]]; then
+        antibody bundle < omz-plugins.txt > "$ZSH_CACHE_DIR/omz-plugins.zsh"
+    fi
+    source "$plugins_file"
+}
+
+# Initialize worker
+async_start_worker comp_worker -n
+async_register_callback comp_worker load_completion
+async_job comp_worker :
--- private_dot_config/zsh/0105-plugins.sh
+++ private_dot_config/zsh/0105-plugins.sh
@@ -1,11 +1,9 @@
-_comp_options+=(globdots) # complete hidden files
-autoload -Uz compinit && compinit
-
 #update plugins when out of date
 plugins_file="$ZSH_CACHE_DIR/plugins.zsh"
 plugins_src="$ZDOTDIR/plugins.txt"
 if [[ ! -e $plugins_file || $plugins_file -ot "$plugins_src" ]] && type antibody >/dev/null; then
-  echo updating "$plugins_file" ...
-  antibody bundle < "$plugins_src" > $plugins_file
+echo updating "$plugins_file" ...
+antibody bundle < "$plugins_src" > $plugins_file
 fi
 source $plugins_file
+

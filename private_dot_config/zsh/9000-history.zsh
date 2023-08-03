b863025d8e20bec8db2efac0a495fe34b73dace4
Scott Schlesier <scotts@readytopay.com>
2023-08-03 11:39:05 -0700

    verify history substituions

--- private_dot_config/zsh/9000-history.zsh
+++ private_dot_config/zsh/9000-history.zsh
@@ -4,4 +4,5 @@ HISTIGNORE='l:gst: '
 setopt HIST_IGNORE_ALL_DUPS  #never duplicate a history command
 setopt HIST_SAVE_NO_DUPS     #never save duplicate commands
 setopt SHARE_HISTORY         #share history between shells
+setopt HIST_VERIFY           #show history substitution before executing
 

6a6f34240a06f24bb0c06921ae08446e2f74c686
Scott Schlesier <scott@schlesier.ca>
2023-10-13 21:39:05 -0700

    skip instant prompt when updating shell scripts

--- private_dot_config/zsh/0005-instant-prompt.zsh
+++ private_dot_config/zsh/0005-instant-prompt.zsh
@@ -1,3 +1,12 @@
+# if doing a script update skip instant prompt this time
+semaphorePath="$ZSH_CACHE_DIR/do_update"
+
+if [[ -e $semaphorePath ]]
+then
+    echo "skipping instant prompt for update"
+    return
+fi
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.

d750c8c02c8873c9dc9ea4d9e233f578ceed1ba9
Scott Schlesier <scotts@readytopay.com>
2025-10-27 12:26:43 -0700

    completion is cached and loaded synchronously

--- private_dot_config/zsh/0100-completion.sh
+++ private_dot_config/zsh/0100-completion.sh
@@ -1,18 +1,11 @@
 # shellcheck shell=bash
 autoload -Uz compinit
 
-#preload async
-async_init=$(echo mafredri/zsh-async | antidote bundle)
-eval "$async_init"
+# Cache completion setup
+# Use zcompdump to cache completions for faster load times
+# The cache is automatically used by compinit and regenerated if needed
+ZSH_COMPDUMP="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompdump-$HOST-$ZSH_VERSION"
+[[ -n $ZSH_COMPDUMP ]] && [[ -d ${ZSH_COMPDUMP:h} ]] || mkdir -p ${ZSH_COMPDUMP:h}
 
-autoload -Uz async && async
-
-function load_completion() {
-    _comp_options+=(globdots) # complete hidden files
-    compinit
-}
-
-# Initialize worker
-async_start_worker comp_worker -n
-async_register_callback comp_worker load_completion
-async_job comp_worker :
+_comp_options+=(globdots) # complete hidden files
+compinit -d "$ZSH_COMPDUMP"

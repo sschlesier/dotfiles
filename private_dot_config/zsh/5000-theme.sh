94a8121d66d30b2517a58cc502e93bbf410e16b1
Scott Schlesier <scotts@readytopay.com>
2025-10-27 12:27:18 -0700

    disable theme if TERM_PROGRAM is vscode
    for cursor etc to not get difficult theme

--- private_dot_config/zsh/5000-theme.sh
+++ private_dot_config/zsh/5000-theme.sh
@@ -3,8 +3,11 @@
 #user for theme to know when to display alternate username
 export "DEFAULT_USER=$(whoami)"
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-if [ -f "$ZDOTDIR/.p10k.zsh" ]; then
+# if in vscode, use a simple prompt
+if [[ $TERM_PROGRAM == "vscode" ]]; then
+  printf "TERM_PROGRAM: %s\n" "$TERM_PROGRAM"
+  PROMPT='%n@%m:%~%# '
+elif [ -f "$ZDOTDIR/.p10k.zsh" ]; then
     # shellcheck source=/dev/null
 	. "$ZDOTDIR/.p10k.zsh"
 fi

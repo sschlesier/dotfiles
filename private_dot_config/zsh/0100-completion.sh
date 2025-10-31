e79c3221ea793d7bc1d00c891773084e2a26479c
Scott Schlesier <scotts@readytopay.com>
2025-10-31 10:39:18 -0700

    searching for shell launch speed

--- private_dot_config/zsh/0100-completion.sh
+++ private_dot_config/zsh/0100-completion.sh
@@ -8,4 +8,4 @@ ZSH_COMPDUMP="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompdump-$HOST-$ZSH_VERSION"
 [[ -n $ZSH_COMPDUMP ]] && [[ -d ${ZSH_COMPDUMP:h} ]] || mkdir -p ${ZSH_COMPDUMP:h}
 
 _comp_options+=(globdots) # complete hidden files
-compinit -d "$ZSH_COMPDUMP"
+compinit -C -d "$ZSH_COMPDUMP"

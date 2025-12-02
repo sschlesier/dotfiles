51fd3f7c24ab78a75bace3f730f8d7c3412270d9
Scott Schlesier <scotts@readytopay.com>
2025-12-02 12:03:57 -0800

    cache antidote prefix dir

--- private_dot_config/zsh/0010-antidote.sh
+++ private_dot_config/zsh/0010-antidote.sh
@@ -1,1 +1,17 @@
-source "$(brew --prefix antidote)/share/antidote/antidote.zsh"
+cache_file="$ZSH_CACHE_DIR/antidote_prefix"
+
+if [[ -z ${ANTIDOTE_PREFIX:-} && -s $cache_file ]]; then
+	ANTIDOTE_PREFIX=$(<"$cache_file")
+fi
+
+if [[ -z ${ANTIDOTE_PREFIX:-} ]]; then
+	ANTIDOTE_PREFIX=$(brew --prefix antidote 2>/dev/null)
+	if [[ -n $ANTIDOTE_PREFIX ]]; then
+		printf '%s' "$ANTIDOTE_PREFIX" >| "$cache_file"
+	fi
+fi
+
+if [[ -n ${ANTIDOTE_PREFIX:-} ]]; then
+	# shellcheck source=/dev/null
+	source "$ANTIDOTE_PREFIX/share/antidote/antidote.zsh"
+fi

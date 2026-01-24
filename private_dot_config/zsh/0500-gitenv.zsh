2d34fb0254770749ccd3d564691658dfa725f84c
Scott Schlesier <scott@schlesier.ca>
2026-01-23 22:05:43 -0800

    more readable approach to gt

--- private_dot_config/zsh/0500-gitenv.zsh
+++ private_dot_config/zsh/0500-gitenv.zsh
@@ -23,7 +23,6 @@ alias grhh='git reset --hard'
 alias gst='git status'
 # \033?7l disables automargin like tput rmam (reset mode auto margin) but works in tmux
 # \033?7h enables automargin like tput smam (set mode auto margin) but works in tmux
-alias gt='printf "\033[?7l$(git tree --color=always | head -n$(($LINES - 4)))\033[?7h\n"'
 alias gta='git tree --all'
 alias gv='git view'
 

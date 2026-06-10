9046702650c48038faf3ea62c29952cb4b26a3b2
Scott Schlesier <scott@schlesier.ca>
2026-06-09 22:09:11 -0700

    hist file to xdg dirs

--- private_dot_config/zsh/9000-history.zsh
+++ private_dot_config/zsh/9000-history.zsh
@@ -1,5 +1,5 @@
 ## History file configuration
-[ -z "$HISTFILE" ] && HISTFILE="$HOME/.zsh_history"
+HISTFILE="${XDG_STATE_HOME:-$HOME/.local/state}/zsh/history"
 [ "$HISTSIZE" -lt 50000 ] && HISTSIZE=50000
 [ "$SAVEHIST" -lt 10000 ] && SAVEHIST=10000
 

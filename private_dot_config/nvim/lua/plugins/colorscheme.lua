8bbfb02a26af8752ff4f76e522c597ce4bdf68f3
Scott Schlesier <scotts@readytopay.com>
2026-07-15 13:31:40 -0700

    nvim light colorscheme uses default line highlight

--- private_dot_config/nvim/lua/plugins/colorscheme.lua
+++ private_dot_config/nvim/lua/plugins/colorscheme.lua
@@ -4,8 +4,19 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme("everforest")
-      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#4f5a6e" })
+      local function apply()
+        vim.cmd.colorscheme("everforest")
+        if vim.o.background == "dark" then
+          vim.api.nvim_set_hl(0, "CursorLine", { bg = "#4f5a6e" })
+        end
+      end
+
+      apply()
+
+      vim.api.nvim_create_autocmd("OptionSet", {
+        pattern = "background",
+        callback = apply,
+      })
     end,
   },
 }
